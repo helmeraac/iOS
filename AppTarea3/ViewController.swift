24fa398e90d224d29aee20d2c0a72666d2843efb
Helmer Avendaño <Helmer@macbook-pro-de-helmer.local>
2016-03-27 23:23:47 -0500

    Initial Commit

---
+++ AppTarea3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppTarea3
+//
+//  Created by Helmer Avendaño on 27/03/16.
+//  Copyright © 2016 Helmer Avendaño. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
